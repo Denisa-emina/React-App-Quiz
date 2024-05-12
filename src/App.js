ec35ad9de501c4c094df8f01041b2274f8a1ec96
Denisa-Emina <99284725+Denisa-emina@users.noreply.github.com>
2024-05-12 20:47:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./App.css";
 import "./styles.css";
-import CountdownTimer from "./components/Timer";
 import { questions } from "./context/Questions";
+import NavButtons from "./context/NavButtons";
+import QuestionsView from "./components/QuestionsView";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
 	
 	// Celelalte functii
+
 	/* un posibil raspuns a fost apasat */
 	const optionClicked = (isCorrect) => {
 		if (isCorrect) {
@@ -42,70 +44,31 @@ function App() {
 			{showResults ? (
 				/*  Rezultate finale */
 				<div className="final-results">
-					<h1>Final Results</h1>
-					<h2>
-						{score} out of {questions.length}
-					</h2>
-					<button onClick={() => restartGame()}>Restart game</button>
-				</div>
+				<h1>Final Results</h1>
+				<h2>
+				  {score} out of {questions.length}
+				</h2>
+				<button onClick={() => restartGame()}>Restart game</button>
+			  </div>
 			) : (
+			  /*  Question Card */
+			  <div className="question-card-container">
 				
-				/*  Question Card */
-				<div className="question-card-container">
-					<div className="question-card">
-						{/* Intrebarea de sus */}
-						<h2>
-							Question: {currentQuestion + 1} out of {questions.length}
-						</h2>
-						<h3 className="question-text">{questions[currentQuestion].text}</h3>
-
-	
-						{/* Raspunsurile posibile */}
-						<ul>
-							{questions[currentQuestion].options.map((option) => {
-								return (
-									<li
-										key={option.id}
-										onClick={() => optionClicked(option.isCorrect)}
-									>
-										{option.text}
-									</li>
-								);
-							})}
-						</ul>
-						<div className="countdown-timer">
-							<CountdownTimer minutes={10} />
-						</div>
-					</div>
-
-
-					{/* Butoanele */}
-					<div className="nav-buttons">
-						<button className="nav-buttons-button"
-							onClick={() => {
-								if (currentQuestion > 0) {
-									setCurrentQuestion(currentQuestion - 1);
-								}
-							}}
-							disabled={currentQuestion === 0}
-						>
-							Prev
-						</button>
-						<button className="nav-buttons-button"
-							onClick={() => {
-								if (currentQuestion + 1 < questions.length) {
-									setCurrentQuestion(currentQuestion + 1);
-								}
-							}}
-							disabled={currentQuestion + 1 === questions.length}
-						>
-							Next
-						</button>
-					</div>
-				</div>
+				<QuestionsView
+				  questions={questions}
+				  currentQuestion={currentQuestion}
+				  optionClicked={optionClicked}
+				/> 
+				
+				<NavButtons
+				  currentQuestion={currentQuestion}
+				  setCurrentQuestion={setCurrentQuestion}
+				  questionsLength={questions.length}
+				/>
+			  </div>
 			)}
-		</div>
-	);
-}
-
-export default App;
+		  </div>
+		);
+	  }
+	  
+	  export default App;
