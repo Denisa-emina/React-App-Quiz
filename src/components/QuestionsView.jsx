103d2f093a19a84918dfbc838e08673135e0c766
Denisa-Emina <99284725+Denisa-emina@users.noreply.github.com>
2024-05-12 20:58:40 +0300

    Add files via upload

---
+++ src/components/QuestionsView.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import CountdownTimer from './Timer';
+
+const QuestionsView = ({ questions, currentQuestion, optionClicked }) => {
+const [timeLeft, setTimeLeft] = useState(10 * 60); 
+  return (
+    <div className="question-card">
+      <h2>
+        Question: {currentQuestion + 1} out of {questions.length}
+      </h2>
+      <h3 className="question-text">{questions[currentQuestion].text}</h3>
+
+      <ul>
+        {questions[currentQuestion].options.map((option) => {
+          return (
+            <li
+              key={option.id}
+              onClick={() => optionClicked(option.isCorrect)}
+            >
+              {option.text}
+            </li>
+          );
+        })}
+      </ul>
+      <CountdownTimer minutes={timeLeft / 60} />
+    </div>
+  );
+};
+
+export default QuestionsView;
---
+++ src/components/Timer.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const CountdownTimer = ({ minutes }) => {
+  const [timeLeft, setTimeLeft] = useState(minutes * 60);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(prevTime => prevTime - 1);
+    }, 1000);
+
+    if (timeLeft === 0) {
+      clearInterval(timer);
+    }
+
+    return () => clearInterval(timer);
+  }, [timeLeft]);
+
+  const formatTime = time => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
+
+  return (
+    <div>
+      
+      <h2><strong>{formatTime(timeLeft)}</strong></h2>
+    </div>
+  );
+};
+
+export default CountdownTimer;
